6e99a33350d47219bea016abd972294abd0232bb
Dhanushan99 <it20051648@my.sliit.lk>
2021-10-28 14:47:47 +0530

    add db helper

---
+++ app/src/main/java/mad/example/sqllite/database/DBHelper.java
@@ -0,0 +1,26 @@
+package mad.example.sqllite.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String Database_Name ="UserIndo.db";
+
+    public DBHelper( Context context) { super(context, Database_Name, null, 1);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
