6b2b6fc5a2c6a2ac94d4d440e020e63e5d994aaf
Dhanushan99 <it20051648@my.sliit.lk>
2021-10-29 19:12:40 +0530

    new

--- app/src/main/java/mad/example/sqllite/MainActivity.java
+++ app/src/main/java/mad/example/sqllite/MainActivity.java
@@ -1,42 +1,140 @@
 package mad.example.sqllite;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import mad.example.sqllite.database.DBHelper;
+
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editname,editpassword;
+
+
+    EditText input1, input2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editname =findViewById(R.id.editname);
-        editpassword =findViewById(R.id.editpassword);
+
+        input1 = findViewById(R.id.input1);
+        input2 = findViewById(R.id.input2);
+
+
     }
 
-    public void saveUser(View view){
-        String name= editname.getText().toString();
-        String password = editpassword.getText().toString();
-        DBHelper dbHelper =new DBHelper(this);
 
-        if (name.isEmpty()||password.isEmpty()){
-            Toast.makeText(this,"enter the values",Toast.LENGTH_LONG).show();
+    public void SaveUser(View view) {
+        DbHelper dbHelper = new DbHelper(this);
+        String Username = input1.getText().toString();
+        String Password = input2.getText().toString();
+
+        if (Username.isEmpty() || Password.isEmpty()) {
+
+
+            Toast.makeText(this, "Empty fields", Toast.LENGTH_SHORT).show();
+
+        } else {
+            long inserted = dbHelper.AddInfo(Username, Password);
+            if (inserted > 0) {
+
+
+                Toast.makeText(this, "Data Inserted", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                Toast.makeText(this, "Data not inserted", Toast.LENGTH_SHORT).show();
+
+
+            }
+
         }
-        else {
-            long inserted = dbHelper.addInfo(name,password);
-        if (inserted >0){
-            Toast.makeText(this," data inserted sucessful",Toast.LENGTH_LONG).show();
+
+
+    }
+
+
+    public void ViewAll(View view) {
+        DbHelper dbHelper = new DbHelper(this);
+
+        List info = dbHelper.ReadAll();
+
+        String[] Listarray = (String[]) info.toArray(new String[0]);
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("USER DETAILS");
+
+        builder.setItems(Listarray, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String Username = Listarray[which].split(":")[0];
+                input1.setText(Username);
+                input2.setText("******");
+            }
+        });
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
+    }
+
+
+    public void deleteInfo(View view) {
+        DbHelper dbHelper = new DbHelper(this);
+
+        String Username = input1.getText().toString();
+
+        if (Username.isEmpty()) {
+
+
+            Toast.makeText(this, "Empty fields", Toast.LENGTH_SHORT).show();
+
+        } else {
+            dbHelper.deleteInfo(Username);
+            Toast.makeText(this, Username + " Is Deleted ", Toast.LENGTH_SHORT).show();
+
         }
-        else {
-            Toast.makeText(this,"somthig went wrong",Toast.LENGTH_LONG).show();
+
+
+    }
+
+
+    public void updateInfo(View view) {
+
+
+        DbHelper dbHelper = new DbHelper(this);
+        String Username = input1.getText().toString();
+        String Password = input2.getText().toString();
+
+        if (Username.isEmpty() || Password.isEmpty()) {
+
+
+            Toast.makeText(this, "Empty fields", Toast.LENGTH_SHORT).show();
+
+        } else {
+            dbHelper.UpdateInfo(view, Username, Password);
         }
-        }
+
 
     }
 }
+
+
+
