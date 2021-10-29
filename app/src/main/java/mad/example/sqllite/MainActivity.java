d782e77adad2e856624c4ffbfd3ba4c887074475
Dhanushan99 <it20051648@my.sliit.lk>
2021-10-29 14:18:54 +0530

    main

--- app/src/main/java/mad/example/sqllite/MainActivity.java
+++ app/src/main/java/mad/example/sqllite/MainActivity.java
@@ -3,12 +3,42 @@ package mad.example.sqllite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import mad.example.sqllite.database.DBHelper;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editname,editpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editname =findViewById(R.id.editname);
+        editpassword =findViewById(R.id.editpassword);
+    }
+
+    public void saveUser(View view){
+        String name= editname.getText().toString();
+        String password = editpassword.getText().toString();
+        DBHelper dbHelper =new DBHelper(this);
+
+        if (name.isEmpty()||password.isEmpty()){
+            Toast.makeText(this,"enter values",Toast.LENGTH_LONG).show();
+
+        }
+        else {
+            long inserted = dbHelper.addInfo(name,password);
+        if (inserted >0){
+            Toast.makeText(this," data insert sucess",Toast.LENGTH_LONG).show();
+
+        }
+        else {
+            Toast.makeText(this,"somthig is wrong",Toast.LENGTH_LONG).show();
+        }
+        }
+
     }
 }
